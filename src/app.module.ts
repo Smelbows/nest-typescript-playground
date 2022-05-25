bac8ef1d94a0b424b909531f4760718e01b16a20
Sarah Mottram <smelbows@gmail.com>
2022-05-25 10:28:50 +0200

    added mongoose and mongo cloud link

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/nest';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoURL)],
   controllers: [AppController],
   providers: [AppService],
 })
